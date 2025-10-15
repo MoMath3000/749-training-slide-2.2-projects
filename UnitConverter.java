5e2e18d36340d3fb2864b373ce506181a21add6a
MoMath3000 <84647351+MoMath3000@users.noreply.github.com>
2025-10-15 15:58:33 -0700

    Create UnitConverter.java

---
+++ UnitConverter.java
@@ -0,0 +1,11 @@
+public class UnitConverter {
+  public static final double metersPerFoot = 0.3048;
+	public static final double degreesPerRadian = 180 / Math.PI;
+	
+  public static double radiansToDegrees(double radians) {
+  	return radians * degreesPerRadian;
+  }
+  public static double feetToMeters(double feet) {
+  	return feet * metersPerFoot;
+  }
+}
